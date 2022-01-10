36906ebbae7f8f2e373006a9d105eff10a163358
ThaiEasyElec <support@thaieasyelec.com>
2022-01-11 00:45:02 +0700

    update Example
    update Example ping,get local time,NTP

---
+++ examples/ex_ping/ex_ping.ino
@@ -0,0 +1,61 @@
+#include "EC2X.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial EC25_SER(8, 9); // RX, TX
+#define POWER_PIN 4
+
+#define NTP_SERVRT_IP "118.175.67.83" //time.navy.mi.th
+
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+
+void setup() {
+  EC25_SER.begin(9600);
+  Serial.begin(9600);
+  Serial.println(F("Hello EC25 LTE 4G Module"));
+  lte.begin(&EC25_SER, POWER_PIN);
+  Serial.println(F("Reset Module"));
+  lte.reset();
+  Serial.print(F("Wait Module Ready"));
+  while (!lte.ready())
+  {
+    Serial.print(F("."));
+    delay(500);
+  }
+  Serial.println();
+  Serial.print(F("SIM Status:"));
+  Serial.println(lte.simStatus() ? "OK" : "Fail");
+  Serial.print(F("IMSI:"));
+  Serial.println(lte.IMSI());
+  Serial.print(F("IMEI:"));
+  Serial.println(lte.IMEI());
+  Serial.print(F("ICCID:"));
+  Serial.println(lte.ICCID());
+  Serial.print(F("Register Network:"));
+  while (!lte.networkRegister())
+  {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println(F("OK"));
+  Serial.print(F("Operator:"));
+  Serial.println(lte.getOperator());
+  Serial.print(F("Signal:"));
+  Serial.println(lte.signal());
+  Serial.print(F("networkInfo:"));
+  Serial.println(lte.networkInfo());
+  Serial.print(F("Connect Internet:"));
+  lte.NET_APN = F("Internet");
+  lte.NET_USERNAME = F("");
+  lte.NET_PASSWORD = F("");
+  Serial.println(lte.connectInternet() ? F("Success") : F("Fail"));
+  Serial.print(F("IP Address:"));
+  Serial.println(lte.IPAddress());
+}
+void loop()
+{
+   //lte.ping("www.google.com",&Serial);
+   String ret = lte.ping("www.google.com");
+   Serial.println(ret);
+}
